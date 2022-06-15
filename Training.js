645e5a5ce7f3ca8d0b7cce6292425ec14be303e8
unknown <momen07996@gmail.com>
2022-06-15 09:48:07 -0700

    training

---
+++ Training.js
@@ -0,0 +1,12 @@
+var cliclk = function () {
+    
+var a = document.getElementById("text").value;
+var s = document.getElementById("password").value;
+
+if(a === "momen" && s === "20210175") {
+
+    document.write("yes");
+    }
+}
+
+document.getElementById("submit2").onclick = cliclk;
